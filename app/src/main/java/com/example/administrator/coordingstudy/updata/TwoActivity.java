79bd050ddb81a20bb12d6ea652f4009651bcce96
qiaoshi <2642656099@qq.com>
2018-07-05 21:00:01 +0800

    测试

---
+++ app/src/main/java/com/example/administrator/coordingstudy/updata/TwoActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.coordingstudy.updata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.administrator.coordingstudy.BaseActivity;
+import com.example.administrator.coordingstudy.R;
+
+public class TwoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        //加载Fragmentfram
+        OneFragment oneFragment = new OneFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fram,oneFragment).commit();
+
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this,ThreeActivity.class));
+    }
+}

780943c9d4b115272775b75c56aca982f734fba6
qiaoshi <2642656099@qq.com>
2018-07-06 11:26:45 +0800

    上传到远程仓库

--- app/src/main/java/com/example/administrator/coordingstudy/updata/ThreeActivity.java
+++ app/src/main/java/com/example/administrator/coordingstudy/updata/ThreeActivity.java
@@ -1,27 +1,23 @@
 package com.example.administrator.coordingstudy.updata;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.administrator.coordingstudy.BaseActivity;
+import com.example.administrator.coordingstudy.Main2Activity;
 import com.example.administrator.coordingstudy.R;
 
-public class TwoActivity extends BaseActivity {
+public class ThreeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
-        //加载Fragmentfram
-        //再次提交
-        OneFragment oneFragment = new OneFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fram,oneFragment).commit();
-
     }
 
     public void onClick(View view) {
-        startActivity(new Intent(this,ThreeActivity.class));
+        startActivity(new Intent(this, Main2Activity.class));
     }
 }
